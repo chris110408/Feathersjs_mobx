1236a36720ca93786fe13b6790eb696845bc5dbd
chrisChen <lei110408@gmail.com>
2020-02-24 08:20:28 -0800

    Finished Login page

---
+++ web/src/pages/login/components/raw-form.js
@@ -0,0 +1,41 @@
+import Form from "mobx-autoform";
+import * as F from "futil";
+import * as _ from "lodash/fp";
+import { validators } from "mobx-autoform";
+import V from "validatorjs";
+import {Input} from '../../../components/input'
+const { validatorJS, functions } = validators;
+
+
+const mergeOver = fns =>
+    _.flow(
+        _.over(fns),
+        F.mergeAllArrays
+    );
+
+const rawForm = {
+    fields: {
+        email: {
+            type: "email",
+            label: "Email Address",
+            component: Input,
+            rules: "required|email|string|between:5,250",
+            value: ""
+        },
+        password: {
+            component: Input,
+            props: { type: "password" },
+            rules: "required"
+        }
+    },
+    submit:async (snapshot)=> {
+        // console.log("submit!", snapshot);
+        return snapshot
+    }
+};
+const LoginForm = Form({
+    ...rawForm,
+    validate: mergeOver([validatorJS(V), functions])
+});
+
+export default LoginForm
--- web/src/pages/login/index.js
+++ web/src/pages/login/index.js
@@ -1,15 +1,29 @@
-import React, { useContext } from "react";
-
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { Form, FormContent, Box, Flex } from "grey-vest";
+import _ from "lodash/fp";
 import styled from "styled-components";
+import { reaction, transaction, extendObservable } from "mobx";
 
 import { observer } from "mobx-react";
+import LoginForm from "./components/raw-form";
+import { CommandButton } from "../../components/command-button";
+import FormField from "../../components/form-field";
 
+import {
+  _Command,
+  extraCommand,
+  redirectSaveTokenCommand
+} from "../../utils/extend-futil";
 import "antd/dist/antd.css";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import image from "../../img/login.svg";
 import  {GobalStoreContext} from "../../store";
 
-
+let Command = extraCommand(
+  _Command,
+  redirectSaveTokenCommand("/", "successful login"),
+  x => y => extendObservable(y, x)
+);
 
 const Divform = styled.div`
  
@@ -31,21 +45,69 @@ const Divform = styled.div`
 }
 
  
-  
+  .form-main {
+    background-color: #f6f9fc;
+    padding: 10px 50px 50px 50px;
+  }
+  .form-field {
+    margin: 10px 20px;
+  }
+
+  .button-group {
+    display: flex;
+    justify-content: space-around;
+    margin-top: 50px;
+  }
 `;
 
-
+transaction(() => {
+  _.each(
+    field => reaction(() => field.value, field.validate),
+    LoginForm.fields
+  );
+});
 
 const Login = () => {
 
   let history = useHistory();
   const GlobalStore = useContext(GobalStoreContext);
+  console.log(GlobalStore)
+  GlobalStore.history = history;
+  useEffect(() => {
+    LoginForm.fields.email.focus();
+  }, []);
 
+  const handleSubmit = Command(async () => {
 
+      if(LoginForm.isValid){
+        const fields = await LoginForm.submit();
+        const res = await GlobalStore.loginEffect(fields);
+        return res
+      }else{
+        throw LoginForm.submitError
+      }
+
+    }
+  );
   return  (
     <Divform>
       <div className='container'>
-       Login
+        <Form as={Box} className='form-main'>
+          <Flex justifyContent='center'>
+            <h1 className='form-header'>Login</h1>
+          </Flex>
+          <FormContent columns={2}>
+            <FormField field={LoginForm.fields.email} width={2} />
+            <FormField field={LoginForm.fields.password} width={2} />
+          </FormContent>
+          {/* Without a submit button, the form won't submit */}
+          <div className='button-group'>
+            <CommandButton command={handleSubmit} arg={LoginForm.isValid} >Submit</CommandButton>
+            <Link className='register' to='/register'>
+              Sign Up
+            </Link>
+          </div>
+        </Form>
       </div>
     </Divform>
   );
