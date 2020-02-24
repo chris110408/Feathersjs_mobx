a61b13be50490e6265ae277092fe0ff3b7cf8cd2
chrisChen <lei110408@gmail.com>
2020-02-24 08:29:22 -0800

    edit user model

--- api/src/models/users.model.js
+++ api/src/models/users.model.js
@@ -6,11 +6,11 @@ module.exports = function (app) {
   const modelName = 'users';
   const mongooseClient = app.get('mongooseClient');
   const schema = new mongooseClient.Schema({
-  
+
     email: { type: String, unique: true, lowercase: true },
     password: { type: String },
-  
-  
+    first_name: {type: String},
+    avatar:{type: String}
   }, {
     timestamps: true
   });
